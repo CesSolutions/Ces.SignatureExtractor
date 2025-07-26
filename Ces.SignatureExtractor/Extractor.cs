dcf46cd1cd0f32cab05c9fbd22f77e86ace04d1e
hadi mazareei <hadi.mazareei@live.com>
2025-07-26 13:55:55 +0330

    finalize method

--- Ces.SignatureExtractor/Extractor.cs
+++ Ces.SignatureExtractor/Extractor.cs
@@ -6,34 +6,57 @@
         {
         }
 
-        public System.Drawing.Bitmap Extract(string imagePath, bool useOriginalColor = true, System.Drawing.Color? customColor = null)
+        /// <summary>
+        /// استخراج خط امضا از تصویر اسکن شده
+        /// </summary>
+        /// <param name="imagePath"></param>
+        /// <param name="useOriginalColor"></param>
+        /// <param name="customColor"></param>
+        /// <param name="backgroundPrecision">اگر پس زمینه تصویر کدر بود این مقدار را کاهش دهید</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public System.Drawing.Bitmap Extract(
+            string imagePath, 
+            bool useOriginalColor = true, 
+            System.Drawing.Color? customColor = null, 
+            byte backgroundPrecision = 200)
         {
             if (useOriginalColor && !customColor.HasValue)
                 throw new ArgumentNullException(nameof(customColor));
 
             // ایجاد تصویر جدید براساس فایل انتخاب شده توسط کاربر
-            var imgOriginal = new System.Drawing.Bitmap(System.Drawing.Image.FromFile(imagePath));
+            using var imgOriginal = new System.Drawing.Bitmap(System.Drawing.Image.FromFile(imagePath));
 
             // ایجاد تصویر نهایی که قرار است نتیجه در آن ترسیم شود
             // این تصویر خالی است و قرار است رسم روی آن انجام شود
-            var imgSignature = new System.Drawing.Bitmap(imgOriginal.Width, imgOriginal.Height);
+            using var imgSignature = new System.Drawing.Bitmap(imgOriginal.Width, imgOriginal.Height);
 
-            // بدلیل آنکه استخراج تصویر با بررسی رنگ پیکسل‌های تصویر انجام می شود
-            // بنابراین یک متغیر جهت نگهداری رنگ هر پیکسل تعریف میکنیم
-            System.Drawing.Color currentPixelColor = default;
-
+            //ابتدا اثر امضا را در تصویر جدید رسم می‌کنیم
             for (int x = 0; x <= (imgOriginal.Width - 1); x++)
             {
                 for (int y = 0; y <= imgOriginal.Height - 1; y++)
                 {
-                    currentPixelColor = imgOriginal.GetPixel(x, y);
+                    var currentPixelColor = imgOriginal.GetPixel(x, y);
 
-                    // ممکن است رنگ خودکار آبی و یا سفید و یا هر رنگ دیگه ای باشد
+                    //دو خط کد پایینی به کمک چت جی‌پی‌تی تولید شده و کارکرد آن ایجاد لبه‌های
+                    //نرم کنار خطوط امضا می باشد که همان مقدار آلفای رنگ است
+                    //در جایگزینی رنگ بجای رنگ اصلی باید میزان روشنایی رنگ قبلی محاسبه شود
+                    //اگر این متغیر بدست نیاید تمام امضا تک رنگ و با یک شدت خواهد بود که
+                    //زیبایی امضا از بین خواهد رفت. بنابراین در زمان جایگزینی رنگ جدید
+                    //باید شدت رنگ قبلی به رنگ جدید اعمال شود
+                    //float brightness = (currentPixelColor.R + currentPixelColor.G + currentPixelColor.B) / 3f / 255f;
+                    float brightness = currentPixelColor.GetBrightness();
+                    byte alpha = (byte)(255 * (1.0f - brightness));
+
+                    // ممکن است رنگ خودکار آبی و یا سفید و یا هر رنگ دیگه‌ای باشد
                     // بنابراین با توجه به اینکه نمونه امضا روی کاغذ سفید انجام خواهد شد
-                    // بنابراین هر رنگی به غیر از سفید به عنوان داده های امضا محسوب خواهد
-                    // شد و برنامه آن را به عنوان امضا از زمینه سفید جدا خواهد کرد                
-                    if (currentPixelColor.R >= 0 & currentPixelColor.R <= 200)
-                        imgSignature.SetPixel(x, y, useOriginalColor ? currentPixelColor : customColor.Value);
+                    // بنابراین هر رنگی به غیر از سفید به عنوان داده‌های امضا محسوب خواهد
+                    // شد و برنامه آن را به عنوان امضا از زمینه سفید جدا خواهد کرد
+                    if (currentPixelColor.R >= 0 & currentPixelColor.R <= backgroundPrecision)
+                        imgSignature.SetPixel(x, y,
+                            useOriginalColor ?
+                            System.Drawing.Color.FromArgb(alpha, currentPixelColor) :
+                            System.Drawing.Color.FromArgb(alpha, customColor.Value));
                 }
             }
 
@@ -45,100 +68,76 @@
             var imgLeftBoundry = 0;
             var imgRightBoundry = 0;
 
-            // مقدار متغیر جهت خروج از حلقه استفاده خواهد شد
-            bool pointFound = false;
-
+            //بدست آوردن مرز بالایی امضا
             for (int y = 0; y <= imgSignature.Height - 1; y++)
             {
                 for (int x = 0; x <= imgSignature.Width - 1; x++)
                 {
                     // اگر رنگ تشخیص داده شود پس مرز بالایی امضا می باشد
-                    if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
-                    {
-                        imgTopBoundry = y;
-                        pointFound = true;
-                        break;
-                    }
-                }
+                    if (imgTopBoundry == 0)
+                        if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
+                            imgTopBoundry = y;
 
-                if (pointFound)
-                    break;
-            }
-
-            pointFound = false;
-
-            for (int y = imgSignature.Height - 1; y >= 0; y -= 1)
-            {
-                for (int x = 0; x <= imgSignature.Width - 1; x++)
-                {
                     // اگر رنگ تشخیص داده شود پس مرز پایینی امضا می باشد
-                    if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
-                    {
-                        imgBottomBoundry = y;
-                        pointFound = true;
+                    if (imgBottomBoundry == 0)
+                        if (imgSignature.GetPixel(x, imgSignature.Height - 1 - y).ToString() != "Color [A=0, R=0, G=0, B=0]")
+                            imgBottomBoundry = imgSignature.Height - 1 - y;
+
+                    if (imgTopBoundry > 0 && imgBottomBoundry > 0)
                         break;
-                    }
                 }
 
-                if (pointFound)
+                if (imgTopBoundry > 0 && imgBottomBoundry > 0)
                     break;
             }
 
-            pointFound = false;
-
+            //بدست آوردن مرز چپ و راست امضا
             for (int x = 0; x <= imgSignature.Width - 1; x++)
             {
                 for (int y = 0; y <= imgSignature.Height - 1; y++)
                 {
                     // اگر رنگ تشخیص داده شود پس مرز چپ امضا می باشد
-                    if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
-                    {
-                        imgLeftBoundry = x;
-                        pointFound = true;
-                        break;
-                    }
-                }
+                    if (imgLeftBoundry == 0)
+                        if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
+                            imgLeftBoundry = x;
 
-                if (pointFound)
-                    break;
-            }
-
-            pointFound = false;
-
-            for (int x = imgSignature.Width - 1; x >= 0; x -= 1)
-            {
-                for (int y = 0; y <= imgSignature.Height - 1; y++)
-                {
                     // اگر رنگ تشخیص داده شود پس مرز راست امضا می باشد
-                    if (imgSignature.GetPixel(x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
-                    {
-                        imgRightBoundry = x;
-                        pointFound = true;
+                    if (imgRightBoundry == 0)
+                        if (imgSignature.GetPixel(imgSignature.Width - 1 - x, y).ToString() != "Color [A=0, R=0, G=0, B=0]")
+                            imgRightBoundry = imgSignature.Width - 1 - x;
+
+                    if (imgLeftBoundry > 0 && imgRightBoundry > 0)
                         break;
-                    }
                 }
 
-                if (pointFound)
+                if (imgLeftBoundry > 0 && imgRightBoundry > 0)
                     break;
             }
 
-            // ایجاد تصویر جدید جهت رسم امضای نهایی
-            // ابعاد تصویر با توجه به موقعیت های بدست آمده در حلقه ها تعیین خواهد شد
-            var imgResultBoundry = new System.Drawing.Bitmap(
-                imgRightBoundry - imgLeftBoundry,
-                imgBottomBoundry - imgTopBoundry);
+            try
+            {
+                // ایجاد تصویر جدید جهت رسم امضای نهایی
+                // ابعاد تصویر با توجه به موقعیت های بدست آمده در حلقه ها تعیین خواهد شد            
+                var imgResultBoundry = new System.Drawing.Bitmap(
+                    imgRightBoundry - imgLeftBoundry,
+                    imgBottomBoundry - imgTopBoundry);
 
-            // ایجاد گرافیک جدید جهت رسم امضا
-            using System.Drawing.Graphics gBoundry = System.Drawing.Graphics.FromImage(imgResultBoundry);
+                // ایجاد گرافیک جدید جهت رسم امضا
+                using System.Drawing.Graphics gBoundry = System.Drawing.Graphics.FromImage(imgResultBoundry);
+                gBoundry.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
-            gBoundry.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            gBoundry.DrawImage(
-                imgSignature,
-                new System.Drawing.Rectangle(0, 0, imgResultBoundry.Width, imgResultBoundry.Height),
-                new System.Drawing.Rectangle(imgLeftBoundry, imgTopBoundry, imgResultBoundry.Width, imgResultBoundry.Height),
-                System.Drawing.GraphicsUnit.Pixel);
+                gBoundry.DrawImage(
+                    imgSignature,
+                    new System.Drawing.Rectangle(0, 0, imgResultBoundry.Width, imgResultBoundry.Height),
+                    new System.Drawing.Rectangle(imgLeftBoundry, imgTopBoundry, imgResultBoundry.Width, imgResultBoundry.Height),
+                    System.Drawing.GraphicsUnit.Pixel);
 
-            return imgResultBoundry;
+                return imgResultBoundry;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
